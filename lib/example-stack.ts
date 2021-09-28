d5c62cf8e096fe15f1832c1ad5385f27155b9159
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-09-28 14:26:59 +0300

    feat: Prepare as an npm package

--- lib/example-stack.ts
+++ lib/example-stack.ts
@@ -3,7 +3,7 @@ import {Pipeline} from "@aws-cdk/aws-codepipeline";
 import {CodePipelinePostToGitHub} from "./CodePipelinePostToGitHub";
 import {StringParameter} from "@aws-cdk/aws-ssm";
 
-export class CdkReportCodepipelineStatusToGithubStack extends cdk.Stack {
+export class ExampleStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
